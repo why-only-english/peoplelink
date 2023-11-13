b4859610b97d3d7bbe463cd603454fa012f49a4f
jgmoon <6813821@tukorea.ac.kr>
2023-11-13 11:21:51 +0900

    초기 설정

---
+++ peoplelink/src/main/java/com/example/peoplelink/MainServlet.java
@@ -0,0 +1,32 @@
+package com.example.peoplelink;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+@WebServlet(name = "mainServlet", value = "/main-servlet")
+public class MainServlet extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "Main Page111!";
+    }
+    Date today = new Date();
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        // Main
+        PrintWriter out = response.getWriter();
+            out.println("<html><body>");
+            out.println("<h1>" + message + today +"</h1>");
+            out.println("</body></html>");
+    }
+
+    public void destroy() {
+    }
+}
