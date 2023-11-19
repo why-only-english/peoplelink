08d9b021205b12f1c4bd0a9d06cb9bbfe1d426d6
jgmoon <6813821@tukorea.ac.kr>
2023-11-19 17:23:36 +0900

    [ADD] POST 추가

---
+++ peoplelink/src/main/java/com/example/peoplelink/post/Post.java
@@ -0,0 +1,59 @@
+package com.example.peoplelink.post;
+
+public class Post {
+
+    private int postID;
+    private String postTitle;
+    private String userID;
+    private String postDate;
+    private String postContent;
+    private int postAvailable;
+
+    public int getPostID() {
+        return postID;
+    }
+
+    public void setPostID(int postID) {
+        this.postID = postID;
+    }
+
+    public String getPostTitle() {
+        return postTitle;
+    }
+
+    public void setPostTitle(String postTitle) {
+        this.postTitle = postTitle;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public String getPostDate() {
+        return postDate;
+    }
+
+    public void setPostDate(String postDate) {
+        this.postDate = postDate;
+    }
+
+    public String getPostContent() {
+        return postContent;
+    }
+
+    public void setPostContent(String postContent) {
+        this.postContent = postContent;
+    }
+
+    public int getPostAvailable() {
+        return postAvailable;
+    }
+
+    public void setPostAvailable(int postAvailable) {
+        this.postAvailable = postAvailable;
+    }
+}
